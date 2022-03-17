e0f5587e1f1205bb9e594874e5a949e47a86b096
Piyush Keshri <piyushkeshri383@gmail.com>
2022-03-17 12:20:29 +0530

    Commit

--- app.js
+++ app.js
@@ -31,6 +31,27 @@ app.get('/api/v1/tours', (req, res) => {
   });
 });
 
+app.get('/api/v1/tours/:id', (req, res) => {
+  console.log(req.params);
+
+  const id = req.params.id * 1;
+  if (id > tours.length) {
+    return res.status(404).json({
+      status: 'Fail',
+      message: 'Invalid Id',
+    });
+  }
+  const tour = tours.find((el) => el.id === id);
+
+  res.status(200).json({
+    status: 'success',
+    results: tours.length,
+    data: {
+      tour,
+    },
+  });
+});
+
 // Post Request
 app.post('/api/v1/tours', (req, res) => {
   // console.log(req.body);
@@ -50,6 +71,22 @@ app.post('/api/v1/tours', (req, res) => {
       });
     }
   );
+});
+
+// Patch Requests
+app.patch('api/v1/tours/:id', (req, res) => {
+  if (req.params.id * 1 > tours.length) {
+    return res.status(404).json({
+      status: 'fail',
+      message: 'Invalid ID',
+    });
+  }
+  res.status(200).json({
+    status: 'success',
+    data: {
+      tour: '<Udated Tour here ...>',
+    },
+  });
 });
 
 const port = 3000;
