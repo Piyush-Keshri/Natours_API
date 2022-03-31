3f4883dce0f47f503eda01935773b39ce6b09403
Piyush Keshri <piyushkeshri383@gmail.com>
2022-03-31 20:46:19 +0530

    Refactoring...

--- controllers/tourControllers.js
+++ controllers/tourControllers.js
@@ -1,15 +1,5 @@
 const Tour = require('./../models/tourModel');
 
-exports.checkBody = (req, res, next) => {
-  if (!req.body.name || !req.body.price) {
-    return res.status(400).json({
-      status: 'fail',
-      message: 'Missing name or price',
-    });
-  }
-  next();
-};
-
 exports.getAllTours = (req, res) => {
   res.status(200).json({
     status: 'success',
@@ -36,13 +26,21 @@ exports.getTour = (req, res) => {
 };
 
 //tours Post Request
-exports.createTour = (req, res) => {
-  res.status(201).json({
-    status: 'success',
-    // data: {
-    //   tour: newTour,
-    // },
-  });
+exports.createTour = async (req, res) => {
+  try {
+    const newTour = await Tour.create(req.body);
+    res.status(201).json({
+      status: 'success',
+      data: {
+        tour: newTour,
+      },
+    });
+  } catch (err) {
+    res.status(400).json({
+      status: 'fail',
+      message: err,
+    });
+  }
 };
 
 //tours Patch Request
