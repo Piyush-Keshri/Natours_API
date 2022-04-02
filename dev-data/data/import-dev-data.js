30ad2aaa7764a758d3458682f85eaaa8776c3a9b
Piyush Keshri <piyushkeshri383@gmail.com>
2022-04-02 17:41:16 +0530

    added script for importing and deleting JSON data for tours

---
+++ dev-data/data/import-dev-data.js
@@ -0,0 +1,56 @@
+const fs = require('fs');
+const dotenv = require('dotenv');
+const mongoose = require('mongoose');
+const Tour = require('./../../models/tourModel');
+
+dotenv.config({ path: './config.env' });
+
+const DB = process.env.DATABASE.replace(
+  '<PASSWORD>',
+  process.env.DATABASE_PASSWORD
+);
+console.log(DB);
+mongoose
+  .connect(DB, {
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useFindAndModify: false,
+  })
+  .then((con) => {
+    console.log(con.connections);
+    console.log('DB connection Successful');
+  });
+
+//   Read JSON file
+
+const tours = JSON.parse(
+  fs.readFileSync(`${__dirname}/tours-simple.json`, 'utf-8')
+);
+
+// Import Data into Database
+const importData = async () => {
+  try {
+    await Tour.create(tours);
+    console.log('Data Successfully loaded!');
+  } catch (err) {
+    console.log(err);
+  }
+  process.exit();
+};
+
+// Delete All Data From Collection
+const deleteData = async () => {
+  try {
+    await Tour.deleteMany();
+    console.log('Data successfully Deleted !');
+  } catch (err) {
+    console.log(err);
+  }
+  process.exit();
+};
+
+if (process.argv[2] === '--import') {
+  importData();
+} else if (process.argv[2] === '--delete') {
+  deleteData();
+}
